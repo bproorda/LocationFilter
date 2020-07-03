31a3a908fa1644e4c7a72a152c14c231ac595ace
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-03 16:50:52 -0500

    userlocation is now being passed back to parent

---
+++ LocationFilter/Models/City.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LocationFilter.Models
+{
+    public class City
+    {
+        public string name { get; set; }
+
+        public locationModel location { get; set; }
+
+    }
+}
