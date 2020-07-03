8a7b3370bcc237e344ba334f15dbf3d80cc43e4c
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-03 18:03:06 -0500

    implemented math methods I need

--- LocationFilter/Models/locationModel.cs
+++ LocationFilter/Models/locationModel.cs
@@ -11,14 +11,28 @@ namespace LocationFilter.Models
 
         public double lng { get; set; }
 
+        public double earthRadius = 3958.8;
+
         public double distance(locationModel cityLocation )
         {
-            return default;
+
+            double latDiff = 69.2 * (this.lat - cityLocation.lat);
+            double lngDiff = lngMiles(this.lng, this.lat) - lngMiles(cityLocation.lng, cityLocation.lat);
+            double squared = Math.Pow(latDiff, 2) + Math.Pow(lngDiff, 2);
+            double distance = Math.Sqrt(squared);
+            return distance;
         }
 
-        public double lngMiles(double lng)
+        public double lngMiles(double lng, double lat)
         {
-            return default;
+            double conversion = (Math.PI / 180) * earthRadius * Math.Cos(degsToRads(lat));
+            double miles = conversion * lng;
+            return miles;
+        }
+        public double degsToRads(double angle)
+        {
+            double answer = (angle * Math.PI) / 180;
+            return answer;
         }
 
     }
