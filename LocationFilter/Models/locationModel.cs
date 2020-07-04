73b4c6cada8aa34567b727cebe3106c493c8371a
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-07-03 20:41:15 -0500

    added CORRECT formula for distance. oops. oh well learned more path

--- LocationFilter/Models/locationModel.cs
+++ LocationFilter/Models/locationModel.cs
@@ -13,7 +13,7 @@ namespace LocationFilter.Models
 
         public double earthRadius = 3958.8;
 
-        public double distance(locationModel cityLocation )
+        public double distance(locationModel cityLocation)
         {
 
             double latDiff = 69.2 * (this.lat - cityLocation.lat);
@@ -34,6 +34,30 @@ namespace LocationFilter.Models
             double answer = (angle * Math.PI) / 180;
             return answer;
         }
+        //https://www.geeksforgeeks.org/haversine-formula-to-find-distance-between-two-points-on-a-sphere/
+        public double haversine(locationModel city)
+        {
+            double lat1 = this.lat;
+            double lon1 = this.lng;
+            double lat2 = city.lat;
+            double lon2 = city.lng;
 
+            // distance between latitudes and longitudes 
+            double dLat = (Math.PI / 180) * (lat2 - lat1);
+            double dLon = (Math.PI / 180) * (lon2 - lon1);
+
+            // convert to radians 
+            lat1 = (Math.PI / 180) * (lat1);
+            lat2 = (Math.PI / 180) * (lat2);
+
+            // apply formulae 
+            double a = Math.Pow(Math.Sin(dLat / 2), 2) +
+                       Math.Pow(Math.Sin(dLon / 2), 2) *
+                       Math.Cos(lat1) * Math.Cos(lat2);
+            double rad = 3958.8;
+            double c = 2 * Math.Asin(Math.Sqrt(a));
+            return rad * c;
+
+        }
     }
 }
